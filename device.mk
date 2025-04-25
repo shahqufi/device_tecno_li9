e8331c61cd4997be2fb61e19b9f3f511683142de
shahqufi <shahqufi@gmail.com>
2025-04-25 19:29:24 +0530

    Add PowerOff Alarm support to device.mk by including the necessary package

--- device.mk
+++ device.mk
@@ -359,6 +359,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
+# PowerOff Alarm
+PRODUCT_PACKAGES += \
+     PowerOffAlarm
+
 
 
 # Sensors
